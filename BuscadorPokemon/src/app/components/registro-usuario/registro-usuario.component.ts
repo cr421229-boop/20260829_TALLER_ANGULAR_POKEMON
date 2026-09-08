937e51b09e8251ea099d2116a4edec3160db93ef
Leydi Carolina Ramírez Rodríguez <cr421229@gmail.com>
2026-09-08 02:04:38 +0000

    Cambio..

--- BuscadorPokemon/src/app/components/registro-usuario/registro-usuario.component.ts
+++ BuscadorPokemon/src/app/components/registro-usuario/registro-usuario.component.ts
@@ -1,11 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+export interface Usuario {
+  id: number;
+  nombreCompleto: string;
+  documento : {
+     tipo : string,
+     numero : string;
+  };
+  LugarNacimiento: {
+    pais: string;
+    ciudad: string;
+  };
+  fecha : string,
+  numero : string,
+  datospersonales : boolean,
+  fechaRegistro: string
+}
+
+
+
 
 @Component({
   selector: 'app-registro-usuario',
-  imports: [],
-  templateUrl: './registro-usuario.component.html',
-  styleUrl: './registro-usuario.component.css'
+  imports: [FormsModule],
+  standalone : true,
+  templateUrl: './registro-usuario.html',
+  styleUrl: './registro-usuario.css',
 })
-export class RegistroUsuarioComponent {
 
+export class RegistroUsuario {
+  nombre = signal('');
+  apellido = signal('');
+  pais = signal('');
+  ciudad = signal('');
+  tipo_documento = signal('');
+  numero_de_identificacion = signal('');
+  fecha_de_nacimiento = signal('');
+  numero_de_celular = signal('');
+  datos_personales = signal(false);
+
+
+    ultimoUsuario =signal<Usuario | null>(null);
+
+    guardarUsuario() {
+      if (!this.datos_personales()) {
+        alert('Debe aceptar la política de datos personales');
+      }  
+    
+      const usuarioCreado: Usuario = {
+        id: Date.now(),
+        nombreCompleto: `${this.nombre()} ${this.apellido()}`,
+        LugarNacimiento: {
+          pais: this.pais(),
+          ciudad: this.ciudad()
+        },
+        documento: {
+          tipo: this.tipo_documento(),
+          numero: this.numero_de_identificacion()
+        },
+        fecha: this.fecha_de_nacimiento(),
+        numero: this.numero_de_celular(),
+        datospersonales: this.datos_personales(),
+        fechaRegistro: new Date().toLocaleDateString()
+      };
+    localStorage.setItem(usuarioCreado.id.toString(), JSON.stringify(usuarioCreado));
+
+    this.ultimoUsuario.set(usuarioCreado);
+  }
 }
+  
+
+
