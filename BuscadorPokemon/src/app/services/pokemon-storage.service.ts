f10cc798134ef83cffc7b070bb1527047312567d
Leydi Carolina Ramírez Rodríguez <cr421229@gmail.com>
2026-09-10 02:04:41 +0000

    creacion de directivas

--- BuscadorPokemon/src/app/services/pokemon-storage.service.ts
+++ BuscadorPokemon/src/app/services/pokemon-storage.service.ts
@@ -16,20 +16,53 @@ export interface PokemonTarjeta {
 
 export class PokemonStorageService {
   private http = inject(HttpClient);
-  private readonly STORA_KEY = 'equipo_pokemon_registrado';
+  private readonly STORAGE_KEY = 'equipo_pokemon_registrado';
 
   misPokemons = signal<PokemonTarjeta[]>([]);
   constructor() {
     this.cargarDesdeStorage();
   }
   private cargarDesdeStorage() {
-    const data = localStorage.getItem(this.STORA_KEY);
+    const data = localStorage.getItem(this.STORAGE_KEY);
     if (data) {
       this.misPokemons.set(JSON.parse(data));
     }
     
-  }
+  };
+  // Buscar un Pokémon en la API 
 
+  buscarEnAPI(nombreOId: string){
+    return this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${nombreOId.toLowerCase()}`);
+    
+  };
+  // Guardar un nuevo Pokémon en el almacenamiento
+
+  guardarPokemon(nuevo: PokemonTarjeta){
+    const actualizados = [...this.misPokemons(), nuevo];
+    this.misPokemons.set(actualizados);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(actualizados));
+
+    
+  };
+  // Actualizar el estado de favorito de un Pokémon
+
+  actualizarFavorito(id: number, ){
+    const actualizados = this.misPokemons().map(poke =>{
+      if(poke.id === id){
+        return{...poke, esFavorito: !poke.esFavorito}
+      }
+      return poke;
+    });
+    this.misPokemons.set(actualizados);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(actualizados));
+  };
+  // Eliminar un Pokémon del almacenamiento
+
+  eliminarPokemon(id: number){
+    const filtrados = this.misPokemons().filter(poke => poke.id !== id);
+    this.misPokemons.set(filtrados);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(filtrados));
+  }
 
   
 }
